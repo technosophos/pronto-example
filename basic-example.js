72ade6f9c1f4f54d0dac300c2dd685038490652c
Matt Butcher <technosophos@gmail.com>
2012-09-08 08:28:04 -0500

    Minor re-org.

--- basic-example.js
+++ basic-example.js
@@ -1,3 +1,10 @@
+/**
+ * This is an example Pronto commandline app.
+ *
+ * It has one route ('example') that runs two commands
+ * (RandomName and then SayHi). It prints 'Hello %name'
+ * to the console.
+ */
 var pronto = require('pronto');
 var RandomName = require('./lib/randomname');
 var SayHi = require('./lib/sayhi');
